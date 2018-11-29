ffa6990d467f7db264a696ff5d9f8ae2c373cc74
Nikhil Kohli <kohli.n@husky.neu.edu>
2018-11-28 22:16:05 -0500

    Added pojo classes and patient panels

---
+++ HealthSpring/src/userinterface/PatientRole/BookAppoinmentJPanel.java
@@ -0,0 +1,203 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinterface.PatientRole;
+
+import Business.Enterprise.Enterprise;
+import Business.Organization.DoctorOrganization;
+import Business.UserAccount.UserAccount;
+import Business.WorkQueue.LabTestWorkRequest;
+import Business.WorkQueue.WorkRequest;
+import java.awt.CardLayout;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author nikhi
+ */
+public class BookAppoinmentJPanel extends javax.swing.JPanel {
+
+    /**
+     * Creates new form BookAppoinmentJPanel
+     */
+    
+    private JPanel userProcessContainer;
+    private DoctorOrganization organization;
+    private Enterprise enterprise;
+    private UserAccount userAccount;
+    /**
+     * Creates new form DoctorWorkAreaJPanel
+     */
+        
+    
+    
+   
+    public BookAppoinmentJPanel(JPanel userProcessContainer, UserAccount account, DoctorOrganization organization, Enterprise enterprise) {
+        initComponents();
+        
+        this.userProcessContainer = userProcessContainer;
+        this.organization = organization;
+        this.enterprise = enterprise;
+        this.userAccount = account;
+       // valueLabel.setText(enterprise.getName());
+        populateRequestTable();
+    
+    }
+
+    public void populateRequestTable(){
+        DefaultTableModel model = (DefaultTableModel) workRequestJTable.getModel();
+        
+        model.setRowCount(0);
+        for (WorkRequest request : userAccount.getWorkQueue().getWorkRequestList()){
+            Object[] row = new Object[4];
+            row[0] = request.getMessage();
+            row[1] = request.getReceiver();
+            row[2] = request.getStatus();
+            String result = ((LabTestWorkRequest) request).getTestResult();
+            row[3] = result == null ? "Waiting" : result;
+            
+            model.addRow(row);
+        }
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        workRequestJTable = new javax.swing.JTable();
+        refreshTestJButton = new javax.swing.JButton();
+        bookAppJButton = new javax.swing.JButton();
+        BookCnfrmlbl = new javax.swing.JLabel();
+        BookCnfrmlbl2 = new javax.swing.JLabel();
+
+        workRequestJTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
+            },
+            new String [] {
+                "HEALTHSPRING CENTRE", "CATEGORY", "DOCTOR", "DATE", "TIME"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(workRequestJTable);
+
+        refreshTestJButton.setText("Refresh");
+        refreshTestJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                refreshTestJButtonActionPerformed(evt);
+            }
+        });
+
+        bookAppJButton.setText("BOOK APPOINTMENT");
+        bookAppJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bookAppJButtonActionPerformed(evt);
+            }
+        });
+
+        BookCnfrmlbl.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        BookCnfrmlbl.setForeground(new java.awt.Color(255, 0, 0));
+
+        BookCnfrmlbl2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        BookCnfrmlbl2.setForeground(new java.awt.Color(255, 0, 0));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(63, 63, 63)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 657, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(312, 312, 312)
+                        .addComponent(bookAppJButton)
+                        .addGap(73, 73, 73)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(BookCnfrmlbl2, javax.swing.GroupLayout.DEFAULT_SIZE, 247, Short.MAX_VALUE)
+                            .addComponent(BookCnfrmlbl, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(60, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(refreshTestJButton)
+                .addGap(116, 116, 116))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addComponent(refreshTestJButton)
+                .addGap(49, 49, 49)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
+                .addComponent(BookCnfrmlbl, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(21, 21, 21)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(bookAppJButton)
+                    .addComponent(BookCnfrmlbl2, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(100, 100, 100))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void refreshTestJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_refreshTestJButtonActionPerformed
+
+        populateRequestTable();
+
+    }//GEN-LAST:event_refreshTestJButtonActionPerformed
+
+    private void bookAppJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bookAppJButtonActionPerformed
+
+           //JLabel mdLabel1 = new JLabel("Your Appointment has been confirmed");
+          // mdLabel1.setFont(new Font("Arial", Font.BOLD, 30)); 
+           //JOptionPane.showMessageDialog(null, mdLabel1);
+           
+           BookCnfrmlbl.setText("Your Appointment has been confirmed");
+           
+           BookCnfrmlbl2.setText("A confirmation email has been sent to you with the Appointment details."
+                   + "You will be redirected to the Home Page in 10 Seconds");
+       
+           // code for timer
+        
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        userProcessContainer.add("PatientWorkAreaJPanel", new PatientWorkAreaJPanel(userProcessContainer, userAccount, organization,enterprise));
+        layout.next(userProcessContainer);
+      //  JPanel userProcessContainer, UserAccount account, DoctorOrganization organization, Enterprise enterprise
+
+    }//GEN-LAST:event_bookAppJButtonActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel BookCnfrmlbl;
+    private javax.swing.JLabel BookCnfrmlbl2;
+    private javax.swing.JButton bookAppJButton;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton refreshTestJButton;
+    private javax.swing.JTable workRequestJTable;
+    // End of variables declaration//GEN-END:variables
+}
